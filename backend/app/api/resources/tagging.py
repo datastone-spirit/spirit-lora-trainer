9e2efb39152e8b874ddd3eaed852c8a0e2186657
tinyzh <dev@ds-serverless-08.cluster.local>
2024-12-11 07:37:22 +0000

    feat: training gen_sh script

--- backend/app/api/resources/tagging.py
+++ backend/app/api/resources/tagging.py
@@ -1,13 +1,8 @@
-from flask import jsonify
-import torch
-import os
-from typing import List, Union
 from flask_restful import Resource, reqparse
-from transformers import AutoProcessor, AutoModelForCausalLM
-from PIL import Image
 from ..common.utils import use_swagger_config,res
 from ..swagger.swagger_config import tag_config,tag_manual_config
 from ..schema.common_valid import tagging_args_valid, manual_tagging_args_valid
+from app.service.tagging import TaggingService
 
 is_processing = False
 
@@ -30,110 +25,14 @@ class Tagging(Resource):
             # todo 增加joycaption模型反推，现在默认florence2
             model_name = args["model_name"]
             image_path = args["image_path"]
-            images = self.load_images_from_directory(image_path)
-            resule = self.run_captioning(images)
+            images = TaggingService().load_images_from_directory(image_path)
+            resule = TaggingService().run_captioning(images)
 
             return res(success=True, data=resule)
         finally:
              is_processing = False
+             return res(success=False, data={})
 
-    def run_captioning(self, image_paths: List[str]) -> List[dict]:
-
-        print("Starting run_captioning...")
-
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"Using device: {device}")
-
-        torch_dtype = torch.float16
-        model = AutoModelForCausalLM.from_pretrained(
-            "multimodalart/Florence-2-large-no-flash-attn",
-            torch_dtype=torch_dtype,
-            trust_remote_code=True,
-            cache_dir="./models/florence2",
-        ).to(device)
-        processor = AutoProcessor.from_pretrained(
-            "multimodalart/Florence-2-large-no-flash-attn",
-            trust_remote_code=True,
-            cache_dir="./models/florence2",
-        )
-
-        results = []
-        output_dir = "captions_output"
-        os.makedirs(output_dir, exist_ok=True)  # 确保输出目录存在
-
-        for image_path in image_paths:
-            print(f"Processing image: {image_path}")
-            
-            # 加载图片
-            image = Image.open(image_path).convert("RGB")
-            
-            # 生成描述
-            prompt = "<DETAILED_CAPTION>"
-            inputs = processor(text=prompt, images=image, return_tensors="pt").to(
-                device, torch_dtype
-            )
-            generated_ids = model.generate(
-                input_ids=inputs["input_ids"],
-                pixel_values=inputs["pixel_values"],
-                max_new_tokens=1024,
-                num_beams=3,
-            )
-            generated_text = processor.batch_decode(
-                generated_ids, skip_special_tokens=False
-            )[0]
-            parsed_answer = processor.post_process_generation(
-                generated_text, task=prompt, image_size=(image.width, image.height)
-            )
-            caption_text = (
-                parsed_answer["<DETAILED_CAPTION>"]
-                .replace("The image shows ", "")
-                .strip()
-            )
-
-            # 保存反推词到文件，与图片文件名一致
-            base_name = os.path.splitext(os.path.basename(image_path))[0]
-            txt_file_path = os.path.join(output_dir, f"{base_name}_caption.txt")
-            with open(txt_file_path, "w", encoding="utf-8") as txt_file:
-                txt_file.write(caption_text)
-
-            # 收集结果
-            results.append(
-                {"image_path": image_path, "caption": caption_text, "txt_path": txt_file_path}
-            )
-
-            print(f"Processed image: {image_path}, Caption saved to {txt_file_path}")
-
-        # 清理模型和缓存
-        model.to("cpu")
-        del model
-        del processor
-        if torch.cuda.is_available():
-            torch.cuda.empty_cache()
-
-        print("run_captioning completed.")
-        return results
-
-    def load_images_from_directory(self,directory: str, extensions=None):
-        """
-        从目录中加载所有图片文件路径。
-
-        参数:
-        - directory (str): 图片所在的目录。
-        - extensions (set): 支持的文件扩展名，默认支持常见图片格式。
-
-        返回:
-        - List[str]: 图片文件路径列表。
-        """
-        if extensions is None:
-            extensions = {".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp"}
-
-        image_paths = []
-        for file in os.listdir(directory):  # 仅遍历当前目录
-            file_path = os.path.join(directory, file)
-            if os.path.isfile(file_path) and os.path.splitext(file)[1].lower() in extensions:
-                image_paths.append(file_path)  # 仅保存路径
-
-        return image_paths
 
     
 class ManualTagging(Resource):
@@ -152,24 +51,6 @@ class ManualTagging(Resource):
             return res(success=False, message="Invalid parameters. `image_path` and `caption_text` are required.")
 
         # 手动打标
-        return self.manual_captioning(image_path, caption_text)
+        return TaggingService().manual_captioning(image_path, caption_text)
 
-    def manual_captioning(self, image_path: str, caption_text: str) -> dict:
-        base_name = os.path.splitext(os.path.basename(image_path))[0]
-        
-        # 输出目录
-        output_dir = "captions_output"
-        os.makedirs(output_dir, exist_ok=True)  # 确保输出目录存在
-
-        # 保存反推词到文件，文件名与图片名一致
-        txt_file_path = os.path.join(output_dir, f"{base_name}_caption.txt")
-        
-        with open(txt_file_path, "w", encoding="utf-8") as txt_file:
-            txt_file.write(caption_text)
-
-        # 返回结果
-        return res(success=True,data={
-            "image_path": image_path,
-            "caption": caption_text,
-            "txt_path": txt_file_path
-        })
+    
--- backend/app/service/train.py
+++ backend/app/service/train.py
@@ -1,13 +1,191 @@
+import os
+import yaml
+from slugify import slugify
+import gradio as gr
+from gradio_logsview import LogsView, LogsViewRunner
 from ..api.model.training_paramter import TrainingParameter
 
-
+MAX_IMAGES = 150
 training_parameters: TrainingParameter
+with open('models.yaml', 'r') as file:
+    models = yaml.safe_load(file)
 
 class TrainingService:
 
     def training(self,parameters :TrainingParameter):
         global training_parameters
         training_parameters = parameters
-        print(f'trainingparameters is {training_parameters}')
-        # TODO: generate shell scripts and run 
+        print(f'trainingparameters is {parameters.output_name} {parameters.class_tokens}')
+        # TODO: generate shell scripts and run
+        sh = self.gen_sh(parameters)
 
+    def resolve_path(self, p):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        norm_path = os.path.normpath(os.path.join(current_dir, p))
+        return f"\"{norm_path}\""
+    def resolve_path_without_quotes(self, p):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        norm_path = os.path.normpath(os.path.join(current_dir, p))
+        return norm_path
+
+    def gen_sh(
+        self,
+        parameters: TrainingParameter
+    ):
+
+        output_dir = self.resolve_path(f"outputs/{parameters.output_name}")
+        sample_prompts_path = self.resolve_path(f"outputs/{parameters.output_name}/sample_prompts.txt")
+
+        line_break = "\\"
+
+        ############# Sample args ########################
+        sample = ""
+        if len(parameters.sample_prompts) > 0 and parameters.sample_every_n_steps > 0:
+            sample = f"""--sample_prompts={sample_prompts_path} --sample_every_n_steps="{parameters.sample_every_n_steps}" {line_break}"""
+
+        if parameters.vram == "16G":
+            # 16G VRAM
+            optimizer = f"""--optimizer_type adafactor {line_break}
+            --optimizer_args "relative_step=False" "scale_parameter=False" "warmup_init=False" {line_break}
+            --lr_scheduler constant_with_warmup {line_break}
+            --max_grad_norm 0.0 {line_break}"""
+        elif parameters.vram == "12G":
+        # 12G VRAM
+            optimizer = f"""--optimizer_type adafactor {line_break}
+            --optimizer_args "relative_step=False" "scale_parameter=False" "warmup_init=False" {line_break}
+            --split_mode {line_break}
+            --network_args "train_blocks=single" {line_break}
+            --lr_scheduler constant_with_warmup {line_break}
+            --max_grad_norm 0.0 {line_break}"""
+        else:
+            # 20G+ VRAM
+            optimizer = f"--optimizer_type adamw8bit {line_break}"
+
+
+        #######################################################
+        model_config = models[parameters.base_model]
+        model_file = model_config["file"]
+        repo = model_config["repo"]
+        if parameters.base_model == "flux-dev" or parameters.base_model == "flux-schnell":
+            model_folder = "models/unet"
+        else:
+            model_folder = f"models/unet/{repo}"
+        model_path = os.path.join(model_folder, model_file)
+        pretrained_model_path = self.resolve_path(model_path)
+
+        clip_path = self.resolve_path("models/clip/clip_l.safetensors")
+        t5_path = self.resolve_path("models/clip/t5xxl_fp16.safetensors")
+        ae_path = self.resolve_path("models/vae/ae.sft")
+        sh = f"""accelerate launch {line_break}
+        --mixed_precision bf16 {line_break}
+        --num_cpu_threads_per_process 1 {line_break}
+        sd-scripts/flux_train_network.py {line_break}
+        --pretrained_model_name_or_path {pretrained_model_path} {line_break}
+        --clip_l {clip_path} {line_break}
+        --t5xxl {t5_path} {line_break}
+        --ae {ae_path} {line_break}
+        --cache_latents_to_disk {line_break}
+        --save_model_as safetensors {line_break}
+        --sdpa --persistent_data_loader_workers {line_break}
+        --max_data_loader_n_workers {parameters.workers} {line_break}
+        --seed {parameters.seed} {line_break}
+        --gradient_checkpointing {line_break}
+        --mixed_precision bf16 {line_break}
+        --save_precision bf16 {line_break}
+        --network_module networks.lora_flux {line_break}
+        --network_dim {parameters.network_dim} {line_break}
+        {optimizer}{sample}
+        --learning_rate {parameters.learning_rate} {line_break}
+        --cache_text_encoder_outputs {line_break}
+        --cache_text_encoder_outputs_to_disk {line_break}
+        --fp8_base {line_break}
+        --highvram {line_break}
+        --max_train_epochs {parameters.max_train_epochs} {line_break}
+        --save_every_n_epochs {parameters.save_every_n_epochs} {line_break}
+        --dataset_config {self.resolve_path(f"outputs/{parameters.output_name}/dataset.toml")} {line_break}
+        --output_dir {output_dir} {line_break}
+        --output_name {parameters.output_name} {line_break}
+        --timestep_sampling {parameters.timestep_sampling} {line_break}
+        --discrete_flow_shift 3.1582 {line_break}
+        --model_prediction_type raw {line_break}
+        --guidance_scale {parameters.guidance_scale} {line_break}
+        --loss_type l2 {line_break}"""
+
+        return sh
+    
+    def gen_toml(
+        self,
+        dataset_folder,
+        resolution,
+        class_tokens,
+        num_repeats
+    ):
+        toml = f"""[general]
+        shuffle_caption = false
+        caption_extension = '.txt'
+        keep_tokens = 1
+
+        [[datasets]]
+        resolution = {resolution}
+        batch_size = 1
+        keep_tokens = 1
+
+        [[datasets.subsets]]
+        image_dir = '{self.resolve_path_without_quotes(dataset_folder)}'
+        class_tokens = '{class_tokens}'
+        num_repeats = {num_repeats}"""
+        return toml
+    
+    def start_training(
+        self,
+        base_model,
+        lora_name,
+        train_script,
+        train_config,
+        sample_prompts,
+    ):
+        # write custom script and toml
+        if not os.path.exists("models"):
+            os.makedirs("models", exist_ok=True)
+        if not os.path.exists("outputs"):
+            os.makedirs("outputs", exist_ok=True)
+        output_name = slugify(lora_name)
+        output_dir = self.resolve_path_without_quotes(f"outputs/{output_name}")
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+
+
+        file_type = "sh"
+
+        sh_filename = f"train.{file_type}"
+        sh_filepath = self.resolve_path_without_quotes(f"outputs/{output_name}/{sh_filename}")
+        with open(sh_filepath, 'w', encoding="utf-8") as file:
+            file.write(train_script)
+        gr.Info(f"Generated train script at {sh_filename}")
+
+
+        dataset_path = self.resolve_path_without_quotes(f"outputs/{output_name}/dataset.toml")
+        with open(dataset_path, 'w', encoding="utf-8") as file:
+            file.write(train_config)
+        gr.Info(f"Generated dataset.toml")
+
+        sample_prompts_path = self.resolve_path_without_quotes(f"outputs/{output_name}/sample_prompts.txt")
+        with open(sample_prompts_path, 'w', encoding='utf-8') as file:
+            file.write(sample_prompts)
+        gr.Info(f"Generated sample_prompts.txt")
+
+        # Train
+        
+        command = f"bash \"{sh_filepath}\""
+
+        # Use Popen to run the command and capture output in real-time
+        env = os.environ.copy()
+        env['PYTHONIOENCODING'] = 'utf-8'
+        env['LOG_LEVEL'] = 'DEBUG'
+        runner = LogsViewRunner()
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        gr.Info(f"Started training")
+        yield from runner.run_command([command], cwd=cwd)
+        yield runner.log(f"Runner: {runner}")
+
+        gr.Info(f"Training Complete. Check the outputs folder for the LoRA files.", duration=None)

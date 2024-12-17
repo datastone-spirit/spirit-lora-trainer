4ff609ae2536320f44c034edf0da58f497543b78
Kun Zhao <zhaokun.km@gmail.com>
2024-12-17 08:38:36 +0000

    Add test case

--- backend/tests/test_task.py
+++ backend/tests/test_task.py
@@ -12,4 +12,29 @@ def test_task_parse_progress_line():
     assert task.detail['remaining'] == '00:00'
     assert task.detail['speed'] == 1.07
     assert task.detail['loss'] == 0.145
+
+def test_task_parse_stdout():
+    task = TrainingTask()
+    task.detail = {}
     
+    # Test Japanese format
+    for line in [
+        "running training / 学習開始",
+        "num train images * repeats / 学習画像の数×繰り返し回数: 10",
+        "num reg images / 正則化画像の数: 0", 
+        "num batches per epoch / 1epochのバッチ数: 10",
+        "num epochs / epoch数: 10",
+        "batch size per device / バッチサイズ: 1",
+        "gradient accumulation steps / 勾配を合計するステップ数 = 1",
+        "total optimization steps / 学習ステップ数: 100"
+    ]:
+        task.parse_stdout(line)
+
+    logger.info(task.detail)
+    assert task.detail['num_train_images'] == 10
+    assert task.detail['num_reg_images'] == 0
+    assert task.detail['num_batches_per_epoch'] == 10
+    assert task.detail['num_epochs'] == 10
+    assert task.detail['batch_size_per_device'] == 1
+    assert task.detail['gradient_accumulation_steps'] == 1
+    assert task.detail['total_optimization_steps'] == 100

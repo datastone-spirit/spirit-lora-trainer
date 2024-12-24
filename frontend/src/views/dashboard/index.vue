7b13f1e6f372d10d75736a28b8a0ed5767665ba5
mulingyuer <1321968423@qq.com>
2024-12-24 16:31:36 +0800

    feat: 调整仪表盘页面src

--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: mulingyuer
  * @Date: 2024-12-04 09:39:40
- * @LastEditTime: 2024-12-23 11:59:33
+ * @LastEditTime: 2024-12-24 16:30:24
  * @LastEditors: mulingyuer
  * @Description: 仪表盘
  * @FilePath: \frontend\src\views\dashboard\index.vue
@@ -9,15 +9,30 @@
 -->
 <template>
 	<div class="dashboard-page">
-		<iframe class="dashboard-page-iframe" src="https://juejin.cn/" title="仪表盘"></iframe>
+		<div class="dashboard-page-content">
+			<iframe class="dashboard-page-iframe" :src="iframeSrc" title="仪表盘"></iframe>
+		</div>
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const path = ref("/tensorboard");
+// const path = ref("https://juejin.cn/");
+const iframeSrc = computed(() => {
+	return `${location.origin}${path.value}`;
+});
+</script>
 
 <style lang="scss" scoped>
 .dashboard-page {
 	height: 100%;
+	padding-right: $zl-padding;
+}
+.dashboard-page-content {
+	height: 100%;
+	background-color: var(--zl-dashboard-bg);
+	border-radius: $zl-border-radius;
+	overflow: hidden;
 }
 .dashboard-page-iframe {
 	display: block;

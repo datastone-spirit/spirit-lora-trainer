cff14fa8985625285bac17d362d1bf5c29866956
tinyzh <dev@ds-serverless-08.cluster.local>
2024-12-24 06:22:51 +0000

    feat: tensorboard

--- backend/app/api/run.py
+++ backend/app/api/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory,Response, request, redirect
+from flask import Flask, send_from_directory,Response, request
 import requests
 from flask_cors import CORS
 from flask_restful import Api
@@ -41,8 +41,10 @@ api.add_resource(Tagging, "/training/tag")  # 打标数据集
 api.add_resource(ManualTagging, "/training/tag_manual")  # 手动打标接口
 api.add_resource(Training, "/training/start")  # 启动训练
 api.add_resource(GpuLog, "/training/gpu_log") # gpu功耗、显存信息
-api.add_resource(CurrentTask, "/tasks/current") #
-api.add_resource(TaskHistory, "/tasks/history") # task历史
+api.add_resource(CurrentTask, "/tasks/current") # gpu功耗、显存信息
+api.add_resource(TaskHistory, "/tasks/history") # gpu功耗、显存信息
+
+
 
 # 处理静态文件（如 .js, .css 等）
 @app.route('/admin/assets/<path:filename>')
@@ -55,10 +57,47 @@ def serve_assets(filename):
 def serve_admin(path):
     return send_from_directory(app.static_folder, 'index.html')  # 返回 SPA 的入口文件
 
-@app.route('/tensorboard')
-def tensorboard():
-    # 返回 TensorBoard 首页
-    return redirect(f"http://127.0.0.1:{args.tb_port}")
+# tensorboard 反向代理
+@app.route('/tensorboard/<path:subpath>', methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"])
+def proxy_tensorboard(subpath):
+    """
+    反向代理 TensorBoard 服务
+    """
+    base_url = request.host.split(':')[0]  # 获取域名部分
+    # 构造目标 URL
+    target_url = f'{base_url}:{args.tb_port}'
+    target_url = f"http://127.0.0.1:6006/{subpath}"  # 添加 http://
+
+    # 根据原始请求构造代理请求
+    method = request.method
+    headers = {key: value for key, value in request.headers if key.lower() != 'host'}
+    data = request.get_data()
+    params = request.args
+
+    # 发起代理请求
+    resp = requests.request(method, target_url, headers=headers, data=data, params=params, stream=True)
+
+    # 处理 HTML 内容，替换相对路径为目标服务路径
+    if 'text/html' in resp.headers.get('Content-Type', ''):
+        content = resp.content.decode("utf-8")
+        # 替换所有以 "/" 开头的相对路径为目标服务路径
+        content = content.replace('="/', f'="http://127.0.0.1:6006/')
+        content = content.replace("='/", f"='http://127.0.0.1:6006/")
+        return Response(content, resp.status_code, resp.headers)
+
+    # 转发响应
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = {key: value for key, value in resp.headers.items() if key.lower() not in excluded_headers}
+
+    return Response(resp.content, resp.status_code, headers)
+
+@app.route('/tensorboard', methods=["GET"])
+def tensorboard_root():
+    """
+    返回 TensorBoard 根路径的重定向
+    """
+    return proxy_tensorboard("")
+
 
 from utils.util import setup_logging
 setup_logging()

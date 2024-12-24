9c056de832ee750eaecb3d13191f041d74b27e78
tinyzh <dev@ds-serverless-08.cluster.local>
2024-12-24 03:54:11 +0000

    feat: add tensorboard url

--- backend/app/api/run.py
+++ backend/app/api/run.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory,Response, request, redirect
+import requests
 from flask_cors import CORS
 from flask_restful import Api
 import argparse
@@ -23,6 +24,13 @@ Swagger(app)
 CORS(app, resources=r"/*")
 api = Api(app, prefix="/api")
 
+# add port argument
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", type=int, default=5002)
+parser.add_argument("--host", type=str, default="0.0.0.0")
+parser.add_argument("--tb_port", type=int, default=6006) # tensorboard-port
+args = parser.parse_args()
+
 api.add_resource(File, "/file")  # 获取目录结构
 api.add_resource(PathCheck, "/path_check")  # 检测目录是否存在
 api.add_resource(TagDirFile, "/tag_dir_file")  # 获取目录中图片和txt文件
@@ -33,28 +41,24 @@ api.add_resource(Tagging, "/training/tag")  # 打标数据集
 api.add_resource(ManualTagging, "/training/tag_manual")  # 手动打标接口
 api.add_resource(Training, "/training/start")  # 启动训练
 api.add_resource(GpuLog, "/training/gpu_log") # gpu功耗、显存信息
-api.add_resource(CurrentTask, "/tasks/current") # gpu功耗、显存信息
-api.add_resource(TaskHistory, "/tasks/history") # gpu功耗、显存信息
+api.add_resource(CurrentTask, "/tasks/current") #
+api.add_resource(TaskHistory, "/tasks/history") # task历史
 
 # 处理静态文件（如 .js, .css 等）
 @app.route('/admin/assets/<path:filename>')
 def serve_assets(filename):
     return send_from_directory(os.path.join(getprojectpath(), 'dist', 'assets'), filename)
 
-# Add static file serving route
-@app.route('/spirit_trainer', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve_static_files(path):
-    if path and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')  # 默认返回 index.html
+# 匹配 /admin 和所有 /admin 开头的路径，返回 index.html
+@app.route('/admin', defaults={'path': ''})
+@app.route('/admin/<path:path>')
+def serve_admin(path):
+    return send_from_directory(app.static_folder, 'index.html')  # 返回 SPA 的入口文件
 
-# add port argument
-parser = argparse.ArgumentParser()
-parser.add_argument("--port", type=int, default=5002)
-parser.add_argument("--host", type=str, default="0.0.0.0")
-args = parser.parse_args()
+@app.route('/tensorboard')
+def tensorboard():
+    # 返回 TensorBoard 首页
+    return redirect(f"http://127.0.0.1:{args.tb_port}")
 
 from utils.util import setup_logging
 setup_logging()

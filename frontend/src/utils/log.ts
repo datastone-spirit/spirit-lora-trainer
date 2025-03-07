e8e64223ebd986e26b85da1d239c0f779d955ffc
mulingyuer <1321968423@qq.com>
2025-03-07 15:58:24 +0800

    feat: 调整log持久化实例

--- frontend/src/utils/log.ts
+++ frontend/src/utils/log.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: mulingyuer
  * @Date: 2025-03-07 15:30:07
- * @LastEditTime: 2025-03-07 15:42:56
+ * @LastEditTime: 2025-03-07 15:56:48
  * @LastEditors: mulingyuer
  * @Description: 日志
  * @FilePath: \frontend\src\utils\log.ts
@@ -10,8 +10,15 @@
 import localforage from "localforage";
 export { serializeError } from "serialize-error";
 
+// 配置 localforage 只使用 localStorage
+const logStorage = localforage.createInstance({
+	driver: localforage.LOCALSTORAGE, // 指定使用 localStorage
+	name: "spirit-lora-trainer", // 可选：数据库名称
+	storeName: "logs" // 可选：存储名称
+});
+
 export class Log {
 	public static log(key: string, value: any) {
-		return localforage.setItem(key, value);
+		return logStorage.setItem(key, value);
 	}
 }
